0811e460fce947719be8fa0e08d552c2ee0d5729
ngocnhi030402 <87535180+ngocnhi030402@users.noreply.github.com>
2021-12-29 15:48:46 +0700

    update adder_seq + tb

--- adder_seq.v
+++ adder_seq.v
@@ -6,11 +6,12 @@ module adder_seq(
     input [1023:0]in1, in2;
     output reg[1023:0]out;
     output reg carry_out;
-    reg carry0, carry1;
+    reg carry0, carry1, carry2;
 
-    reg [255:0]r0_0, r0_1;
+    reg[255:0]r0_0, r0_1;
     reg[255:0]r1_0, r1_1;
-    reg[511:0]r2_0, r2_1;
+    reg[255:0]r3_0, r3_1;
+    reg[255:0]r2_0, r2_1;
     
 
     always @(posedge clk) begin
@@ -18,13 +19,19 @@ module adder_seq(
         r0_1 <= in2[255:0];
         r1_0 <= in1[511:256];
         r1_1 <= in2[511:256];
-        r2_0 <= in1[1023:512];
-        r2_1 <= in2[1023:512];
+        r2_0 <= in1[767:512];
+        r2_1 <= in2[767:512];
+        r3_0 <= in1[1023:768];
+        r3_1 <= in2[1023:768];
 
 
         {carry0, out[255:0]} <= r0_0 + r0_1;
+        $display("out:%d", out);
         {carry1, out[511:256]} <= r1_0 + r1_1 + carry0;
-        {carry_out, out[1023:512]} <= r2_0 + r2_1 + carry1;
+         $display("out:%d", out);
+        {carry2, out[767:512]} <= r2_0 + r2_1 + carry1;
+         $display("out:%d", out);
+        {carry_out, out[1023:768]} <= r3_0 + r3_1 + carry2;
     end
 
 endmodule
